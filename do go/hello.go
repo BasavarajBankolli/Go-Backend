ea593e621c11acba46c38130077db56fa8677796
Basavaraj PB <basavarajbankolli76@gmail.com>
2026-02-27 22:13:50 +0530

    hello world prgm

---
+++ do go/hello.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("hello") // print normal print func 
+	fmt.Println()
+
+	name := "Basavaraj"
+	age := 25
+	fmt.Printf("%s's age is %d", name, age)	// printf used to format the o/p
+	println()  
+
+	fmt.Println("hey")  // println used to print in new line 
+}	
