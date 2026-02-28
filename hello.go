f8c8efaf19a0f9742788b740a9c30d91fcb9896f
Basavaraj PB <basavarajbankolli76@gmail.com>
2026-02-28 20:34:44 +0530

    types in go

--- hello.go
+++ hello.go
@@ -14,24 +14,5 @@ func main() {
 	println()  
 
 	fmt.Println("hey")  // println used to print in new line 
-
-
-	var chr rune = 'r'
-
-	fmt.Printf("Type : %T\n", chr)
-	fmt.Printf("Value: %d\n", chr)
-	fmt.Printf("Letter: %c\n", chr)
-
-	var byte byte = 'c'
-	fmt.Printf("Char: %c\n", byte)
-
-	var num uint = 100
-	var pie float32 = 3.14321
-	val := 1000
-
-	fmt.Printf("number %d, pie value %f and another number %d", num, pie, val)
-
-
-
-
+	
 }	
